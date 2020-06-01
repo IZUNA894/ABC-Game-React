4d15924f47e8df6a4aca14544cbce1cb42800203
IZUNA894 <48329138+IZUNA894@users.noreply.github.com>
2020-06-01 23:44:12 +0530

    Updates

---
+++ precache-manifest.a7d2e2b0ba4575951f07d8ed1c4f8ea8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30f3b5ac70924455030727f5e3d97fdc",
+    "url": "/ABC-Game-React/index.html"
+  },
+  {
+    "revision": "438d355ad8596fbcf4d7",
+    "url": "/ABC-Game-React/static/css/main.a6c16444.chunk.css"
+  },
+  {
+    "revision": "b61f302d7db8c594f923",
+    "url": "/ABC-Game-React/static/js/2.b6d739a1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ABC-Game-React/static/js/2.b6d739a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "438d355ad8596fbcf4d7",
+    "url": "/ABC-Game-React/static/js/main.1d3652b5.chunk.js"
+  },
+  {
+    "revision": "464486b5420461a3ba6f",
+    "url": "/ABC-Game-React/static/js/runtime-main.4eaca08f.js"
+  }
+]);
